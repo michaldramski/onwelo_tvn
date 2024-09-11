37871c68d5dbf701e7d7839bc33473e5840ef4ff
Michal Dramski <michal.dramski@gmail.com>
2024-09-11 20:55:27 +0200

    Adding eslint

---
+++ eslint.config.ts
@@ -0,0 +1,47 @@
+import pluginJs from '@eslint/js';
+import eslintPluginPlaywright from 'eslint-plugin-playwright';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+    {
+        ignores: [
+            'package-lock.json',
+            'playwright-report/**',
+            'test-results/**',
+        ],
+    },
+    { files: ['**/*.ts'] },
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            'no-console': 'error',
+        },
+    },
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+          'no-console': 'warn',
+            '@typescript-eslint/explicit-function-return-type': 'error',
+            'prettier/prettier': 'warn',
+            'playwright/no-nested-step': 'off',
+            'playwright/no-wait-for-selector': 'off',
+        },
+    },
+    eslintPluginPlaywright.configs['flat/recommended'],
+    {
+        rules: {
+            'playwright/no-nested-step': 'off',
+        },
+        settings: {
+            playwright: {
+                globalAliases: {
+                    test: ['setup'],
+                },
+            },
+        },
+    },
+    eslintPluginPrettierRecommended,
+];
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,32 +1,32 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
-  timeout: 10 * 1000,
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 1 : undefined,
+    testDir: './tests',
+    timeout: 10 * 1000,
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 1 : 0,
+    workers: process.env.CI ? 1 : undefined,
 
-  reporter: 'html',
-  use: {
-    trace: 'on-first-retry',
-  },
-
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+    reporter: 'html',
+    use: {
+        trace: 'on-first-retry',
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-  ],
+        // {
+        //   name: 'firefox',
+        //   use: { ...devices['Desktop Firefox'] },
+        // },
+
+        // {
+        //   name: 'webkit',
+        //   use: { ...devices['Desktop Safari'] },
+        // },
+    ],
 });
